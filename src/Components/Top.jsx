60e2b84eecd7d0d917e2ae4bc9b53346df0e02de
yahyatoumi <yahyatoumi555@gmail.com>
2023-08-03 16:38:41 +0200

    101

--- src/Components/Top.jsx
+++ src/Components/Top.jsx
@@ -2,7 +2,7 @@ import { BiSolidCameraMovie, BiMoviePlay, BiSearch, BiStar, BiSolidUser, BiPlay 
 
 const Top = () => {
     return (
-        <div className='h-screen ml-0 sm:ml-16 sm:w-vw-16 sm:h-[60vh] min-h-[400px]'>
+        <div className='h-screen ml-0 sm:ml-16 sm:w-vw-16 sm:h-[60vh] sm:min-h-[400px]'>
       <nav className='hidden absolute left-0 pt-3 w-16 sm:flex flex-col gap-5 items-center h-[200vh]'>
         <BiSolidCameraMovie className=' w-10 h-10 text-red-600' />
         <BiSearch className=' w-6 h-6 text-white' />
@@ -12,12 +12,12 @@ const Top = () => {
       </nav>
       <BiSolidCameraMovie className='block absolute left-3 top-3 w-10 h-10 text-red-600 sm:hidden' />
       <div
-        className="bg-cover bg-center bg-no-repeat h-2/5 sm:h-[60vh] min-h-[400px]"
+        className="bg-cover bg-center bg-no-repeat h-2/5 sm:h-[60vh] sm:min-h-[400px]"
         style={{
           backgroundImage: "url('https://hervehubert.tv/wp-content/uploads/2019/10/Logo-full-hd-1.jpg')",
         }}
       ></div>
-      <div className='bg-black sm:bg-transparent text-white min-h-[400px] h-3/5 p-5 pb-10 flex flex-col gap-5 sm:absolute sm:top-0 sm:bg-gradient-to-br sm:from-black sm:to-transparent  sm:w-vw-16 sm:h-[60vh]'>
+      <div className='bg-black sm:bg-transparent text-white sm:min-h-[400px] h-3/5 p-5 pb-10 flex flex-col gap-5 sm:absolute sm:top-0 sm:bg-gradient-to-br sm:from-black sm:to-transparent  sm:w-vw-16 sm:h-[60vh]'>
         <h2 className='font-bold text-4xl '>SAM AWAY</h2>
         <p className='text-xs  font-thin text-gray-400'>Adventure, Fantasy  |  2019  |  136 Min.</p>
         <p className='max-w-xs text-md  font-extralight'>When a tornado hits through City of Peaceville, Samantha (Jenny Loifer) and her dog, Ricko, are whisked away in their house to an amazing journey.</p>
